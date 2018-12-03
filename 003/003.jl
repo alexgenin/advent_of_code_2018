639c306e9e76288c994f4c73dc6b8ae63fa3982a
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-12-03 14:31:03 +0100

    Complete day 3

--- 003/003.jl
+++ 003/003.jl
@@ -11,27 +11,24 @@ using Pkg
 # Pkg.add("DataFrames")
 # Pkg.add("CSV")
 # Pkg.add("StatsBase")
-# Pkg.add("Plots")
 using DataFrames 
 using CSV
 using StatsBase
-using Plots
-using StatPlots
 
 # Read input data 
 f = open("./input.dat")
 dat_vec = readlines(f)
-
-
-# First part
-# ----------
-
 
 # Build a parser for the claims 
 function claimparse(str) 
   numbers = split(str[2:length(str)], r"( @ |,|: |x)")
   return parse.(Int, numbers)
 end
+
+
+
+# First part
+# ----------
 
 # 1000 is the maximum fabric size
 fabric = zeros(Int, 1000, 1000)
@@ -53,23 +50,27 @@ count(x -> x > 1, fabric)
 # Second part
 # -----------
 
-# 1000 is the maximum fabric size
+# 1000 is the maximum fabric size. 
 fabric = zeros(Int, 1000, 1000)
+
+overlaps = falses(1, length(dat_vec))
 
 for i = 1:length(dat_vec) 
   claim = claimparse(dat_vec[i])
   for x = (claim[2]+1):(claim[2]+claim[4])
     for y = (claim[3]+1):(claim[3]+claim[5])
+      if fabric[x,y] > 0
+        overlaps[claim[1]] = true
+        overlaps[fabric[x,y]] = true
+      end
+      # We store in the fabric matrix the ID of the claim (an Int)
       fabric[x,y] = claim[1]
     end
   end
 end
 
-# Number of places where things overlap
-areas = collect(countmap(vec(fabric)))
-areas = hcat(map(x -> x[1], areas), 
-             map(x -> x[2], areas))
+# Check that there is only one fabric that does not overlap 
+count(.!overlaps)
 
-
-
-
+# Get the index (claim ID) of that value
+findfirst( .!overlaps) # 1260
